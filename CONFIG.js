559e7cd55cef20ca7642062360fffa5f340502ea
thienbao2507 <truongphanthienbao2507@gmail.com>
2024-03-06 13:40:30 +0700

    first time

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Chúc cậu 8/3 vui vẻ",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Tớ có đẹp trai hok !!! </br></br></br>Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
+  buttonYes: "Tớ đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn cậu.",
+  descriptionModar:
+    "Hãy cùng tận hưởng ngày của phụ nữ, ngày của phái đẹp. Hôm nay, mình chúc cho Trân sẽ là cô gái xinh đẹp nhất.</br> Love you <3",
+};
