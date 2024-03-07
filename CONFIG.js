5a1c80c7164a837a7bda0f7dde7eb4415b7897f5
thienbao2507 <108049743+thienbao2507@users.noreply.github.com>
2024-03-07 22:53:11 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,7 +1,7 @@
 const CONFIGDATA = {
   titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Chúc cậu 8/3 vui vẻ",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Tớ có đẹp trai hok !!! </br></br></br>Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
+    "Hãy trả lời một cách thật lòng nhé. Cậu thích tớ đúng hok (giả bộ bấm không) !!! </br></br></br>Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
   buttonYes: "Tớ đồng ý",
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn cậu.",
